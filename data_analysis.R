bd7c7f6cc60aee389865023208132fc3fed92f70
Evan Moore <Emoore18@massmutual.com>
2019-04-30 10:13:30 -0400

    add data for two new participants

--- data_analysis.R
+++ data_analysis.R
@@ -1,6 +1,7 @@
 require(googlesheets)
 require(tidyverse)
 require(ggpubr)
+require(lubridate)
 
 p1 <- "Sea turtles are large, air-breathing reptiles that inhabit tropical and subtropical seas throughout the world. Their shells consist of an upper part (carapace) and a lower section (plastron). Hard scales (or scutes) cover all but the leatherback, and the number and arrangement of these scutes can be used to determine the species.
 Sea turtles come in many different sizes, shapes and colors. The olive ridley is usually less than 100 pounds, while the leatherback typically ranges from 650 to 1,300 pounds! The upper shell, or carapace, of each sea turtle species ranges in length, color, shape and arrangement of scales.
@@ -17,15 +18,16 @@ ubitalk <- gs_title("UbiTalk User Study")
 ubitalk <- ubitalk %>%
   gs_read() 
 
-user_data <- ubitalk[1:7, 1:10] %>% 
-  filter(Name != "Katie House") %>% 
-  mutate(control_time = ifelse(control_min == 1, 60 + control_sec, control_sec),
+user_data <- ubitalk[2:9, 1:10] %>% 
+        mutate(control_time = ifelse(control_min == 1, 60 + control_sec, control_sec),
         treatment_time = ifelse(treatment_min == 1, 60 + treatment_sec, treatment_sec),
         wpm_S = (60/S_sec)*len_p2,
         wpm_N = (60/N_sec)*len_p2,
         wpm_F = (60/F_sec)*len_p2,
-        wpm_treatment = ifelse(treatment_time <= 60, (treatment_time/60)*len_p1, (60/treatment_time)*len_p1),
-        wpm_control = ifelse(control_time <= 60, (control_time/60)*len_p1, (60/control_time)*len_p1)
+        wpm_treatment = ifelse(treatment_time < 60, (treatment_time/60)*len_p1, (60/treatment_time)*len_p1),
+        wpm_control = ifelse(control_time < 60, (control_time/60)*len_p1, (60/control_time)*len_p1),
+        gender = ifelse(Name %in% c("Alex", "Quentin", "Colin"), "male", "female"),
+        native_speaker = ifelse(Name %in% c("Isha", "Deepshikha", "Quentin", "Esrath"), "no", "yes")
   ) %>% 
   select("Fast" = wpm_F, "Normal" = wpm_N, "Slow" = wpm_S, everything(), -c(treatment_min, treatment_sec, control_min, control_sec))
 
@@ -38,7 +40,8 @@ app_data <- read_csv("data/final_data.csv")
 #fix anova location
 p <- user_data %>% 
   gather("speed", "wpm", 1:3) %>% 
-  ggboxplot(x = "speed", y = "wpm",
+  ggboxplot(x = "speed", y = "wpm", 
+            add = "jitter",
             color = "speed", palette = "jco") +
   stat_compare_means(method = "anova")
 ggpar(p, 
@@ -51,13 +54,79 @@ ggpar(p,
 p <- user_data %>% 
   gather("group", "wpm", 12:13) %>% 
   ggboxplot(x = "group", y = "wpm",
+            add = "jitter",
             color = "group", palette = "jco") +
-  stat_compare_means(method = "t.test")
+  stat_compare_means()
 ggpar(p, 
       legend = "none",
       main = "WPM by Group",
       xlab = "Group",
       ylab = "Words Per Minute")
 
+#gender differences 
+p <- user_data %>% 
+  gather("group", "wpm", 12:13) %>% 
+  ggboxplot(x = "gender", y = "wpm",
+            color = "gender", palette = "jco",
+            add = "jitter",
+            facet.by = "group", short.panel.labs = FALSE) +
+  stat_compare_means()
+ggpar(p)
+
+#native speaker differences 
+p <- user_data %>% 
+  gather("group", "wpm", 12:13) %>% 
+  ggboxplot(x = "native_speaker", y = "wpm",
+            color = "native_speaker", palette = "jco",
+            add = "jitter",
+            facet.by = "group", short.panel.labs = FALSE) +
+  stat_compare_means()
+ggpar(p)
+
+#speed differences 
+user_data %>% 
+  group_by(Name) %>% 
+  summarise(high_med = Fast - Normal,
+            med_low = Normal - Slow,
+            high_low = Fast - Slow) %>% 
+  gather("difference", "wpm", 2:4) %>%
+  ggplot(aes(x = Name, y = wpm, fill = difference)) + 
+  geom_bar(stat = "identity", position = "dodge")
+
 #APP DATA 
+app_data <- app_data %>%
+  group_by(participant) %>%
+  mutate(time = dplyr::row_number())
+
+#arrange by percentage on pace
+app_data %>% 
+  group_by(participant) %>% 
+  summarize(pct.slow = mean(speed == "Slow"),
+            pct.on_pace = mean(speed == "On_pace"),
+            pct.fast = mean(speed == "Fast")) %>% 
+  gather("speed", "percent", 2:4) %>%
+  arrange(percent) %>% 
+  ggplot(aes(x = participant, y = percent, fill = speed)) + 
+  geom_bar(stat = "identity")
+
+#words over time - not interesting
+app_data %>% 
+  ggplot(aes(x = time, y = n_words_total)) + 
+  geom_line(group = 1) + 
+  facet_grid(participant~.)
+
+#states over time
+app_data %>% 
+  group_by(time, participant) %>% 
+  mutate(speed = case_when(
+    speed == "Slow" ~ -1,
+    speed == "On_pace" ~ 0,
+    TRUE ~ 1
+  )) %>% 
+  ggplot(aes(x = time, y = speed)) + 
+  geom_line() + 
+  facet_grid(participant~.)
+
+#linear regression - words by time (too boring to model) 
+#mod <- lm(n_words_total ~ time, app_data)
   
